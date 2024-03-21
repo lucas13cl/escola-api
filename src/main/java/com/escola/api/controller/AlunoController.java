aa58e986086bc8b3bda8945b05c74c694bcc324e
Lucas Cabral <lucas13cl@outlook.com>
2024-03-20 21:31:15 -0300

    Atualização de documentos

---
+++ src/main/java/com/escola/api/controller/AlunoController.java
@@ -0,0 +1,10 @@
+package com.escola.api.controller;
+
+
+
+
+@RestController
+@RequestMapping(name = "/alunos")
+public class AlunoController {
+
+}
--- src/main/java/com/escola/api/model/Aluno.java
+++ src/main/java/com/escola/api/model/Aluno.java
@@ -1,5 +1,30 @@
 package com.escola.api.model;
 
-public class Aluno {
+    @Data
+    @NoArgsConstructor
+    @AllArgsContructor
+    @Entity(name = "alunos'")
 
-}
+public class Aluno {
+    
+    
+        @id
+        @GeneratedValue(strategy = GenerationType.INDENTITY)
+        private long idAluno;
+        
+        @column(nullable = false, lenght = 50)
+        private String nome;
+    
+        @column(nullable = false, lenght = 150)
+        private String sobrenome;
+    
+        @column(nullable = false, unique = true)
+        private String email;
+        
+        @JsonFormat(pattern = "dd/MM/yyy")
+        private LocalDate dataNascimento;
+    
+    }
+    
+
+
