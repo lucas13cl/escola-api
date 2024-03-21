846300347e0f29890ac4f16d7101574e8a533325
Lucas Cabral <lucas13cl@outlook.com>
2024-03-20 21:46:03 -0300

    ATT 2

--- src/main/java/com/escola/api/controller/AlunoController.java
+++ src/main/java/com/escola/api/controller/AlunoController.java
@@ -1,5 +1,7 @@
 package com.escola.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
@@ -7,4 +9,11 @@ package com.escola.api.controller;
 @RequestMapping(name = "/alunos")
 public class AlunoController {
 
+    @PostMapping
+    public Aluno cadastrarAluo(@RequestBody Aluno aluno){
+        return AlunoRepository.save(aluno);
+    }
+
+    @Autowired
+    private AlunoRepository alunoRepository;
 }
